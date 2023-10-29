057c6f861e903ecefefd303bfcc1ae1221b45da4
Alex Nascimento <alexnnascimentogmail.com>
2023-10-29 12:20:36 -0300

    Adicionando backend s3

--- criando-infra/vpc/provider.tf
+++ criando-infra/vpc/provider.tf
@@ -1,6 +1,13 @@
 #terraform {
 #  required_version = "1.1.9"
 #}
+terraform {
+  backend "s3" {
+    bucket = "desmondlab-tfstate"
+    key    = "tfstate-vpc"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
